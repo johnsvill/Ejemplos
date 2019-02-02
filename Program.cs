6916d7f1237f33e0fe530aa1220dd568ea7d4cf3
Jonathan Villeda <johnsvill@hotmail.com>
2019-02-02 12:19:17 -0600

    Subiendo lineas de saludo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejemplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienvenido al curso de .NET!");
+        }
+    }
+}
